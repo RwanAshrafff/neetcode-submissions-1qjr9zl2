ccf7cdc86cabd93c32902f6d5e0edd1ac7c2ac68
Rwan Ashraf <143334948+RwanAshrafff@users.noreply.github.com>
2026-04-25 19:11:24 +0300

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size(),maxi=0;
+        for(int i=0;i<n;i++){
+            if(maxi<i) return false;
+            maxi=max(maxi,i+nums[i]);
+            if(maxi>=(n-1)) return true;
+        }
+        return true;
+    }
+};
